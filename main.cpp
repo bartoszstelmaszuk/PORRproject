67c9d2b881d0930eb9888cd70a0275822d1d363f
unknown <stelmaszuk.b@gmail.com>
2015-11-28 19:36:34 +0100

    Added block devision function

--- main.cpp
+++ main.cpp
@@ -2,25 +2,51 @@
 #include <stdlib.h>
 #include <math.h>
 #include <vector>
+#include <stdio.h>
+#include <string.h>
 
 using namespace std;
 
 int n = 2;
 int devisionConstant = 2;
 
-struct Point
+typedef struct Point
 {
-    double *coordinates = (double *)malloc(n*sizeof(double));
+    double *coordinates;
 }Point;
 
-struct weightPoint
+typedef struct weightPoint
 {
     double approxFunctionValue;
     vector<double>dimensionsValue;
-    vector<struct Point> blockCorners;
+    vector<Point> blockCorners;
 }weightPoint;
 
-double einstainSummation(struct weightPoint value)
+Point *newPoint ()
+{
+    Point *retPoint = (Point *)malloc(sizeof(Point));
+    if (retPoint == NULL)
+        return NULL;
+
+    retPoint->coordinates = (double *)malloc(n*sizeof(double));
+    if (retPoint->coordinates == NULL) {
+        free (retPoint);
+        return NULL;
+    }
+
+    return retPoint;
+}
+
+struct weightPoint *newWeightPoint ()
+{
+    weightPoint *retPoint = (weightPoint*)malloc(sizeof(weightPoint));
+    if (retPoint == NULL)
+        return NULL;
+
+    return retPoint;
+}
+
+double einstainSummation(weightPoint value)
 {
     double sum=0;
 
@@ -31,7 +57,7 @@ double einstainSummation(struct weightPoint value)
     return sum;
 }
 
-double einstainMultiplication(struct weightPoint value)
+double einstainMultiplication(weightPoint value)
 {
     double result=1;
 
@@ -42,7 +68,7 @@ double einstainMultiplication(struct weightPoint value)
     return result;
 }
 
-double evaluationFunction(struct weightPoint value)
+double evaluationFunction(weightPoint value)
 {
     return (1/40)*einstainSummation(value)+1-einstainMultiplication(value);
 }
@@ -53,10 +79,11 @@ double evaluationFunction(struct weightPoint value)
 
 }*/
 
-struct weightPoint initFunctionDomain()
+weightPoint initFunctionDomain()
 {
-    struct weightPoint initialPoint;
+    weightPoint initialPoint;
 
+    cout << "--------------" << endl;
     double *initValues = (double *)malloc(n*sizeof(double));
     for(int i=0; i<n; i++){
         initValues[i] = 0;
@@ -67,81 +94,140 @@ struct weightPoint initFunctionDomain()
 
 }
 
-struct weightPoint *devideBlock(struct weightPoint point)
+void showBlockCorners(weightPoint weightPoint)
+{
+    struct Point newPoint;
+
+    for(int i=0;i<weightPoint.blockCorners.size();i++){
+        newPoint = weightPoint.blockCorners[i];
+        cout << "weightPoint corner newPoint" << i<< " x: " << newPoint.coordinates[0] << endl;
+        cout << "weightPoint corner newPoint" << i << " y: " << newPoint.coordinates[1] << endl;
+    }
+}
+
+void showPoint(Point selectedPoint)
+{
+    cout << "Point x: " << selectedPoint.coordinates[0] << endl;
+    cout << "Point y: " << selectedPoint.coordinates[1] << endl;
+}
+
+void copy_Point(Point *p1, Point *p2)
+{
+    for(int i=0; i<n; i++) {
+        p1->coordinates[i] = p2->coordinates[i];
+    }
+}
+
+void copy_weightPoint(weightPoint *p1, weightPoint *p2)
+{
+    for(int i=0; i < p2->blockCorners.size() ;i++) {
+            Point *point = newPoint();
+            copy_Point(point, &(p2->blockCorners[i]));
+            p1->blockCorners.push_back(*point);
+    }
+}
+
+vector<weightPoint *> devideBlock(weightPoint *point)
 {
     double longestDistance = 0;
-    struct Point beginFinalPoint;
-    struct Point endFinalPoint;
+    struct Point *firstDevisionPoint = newPoint();
+    struct Point *secondDevisionPoint = newPoint();
+    struct Point *beginPoint = newPoint();
+    struct Point *endPoint = newPoint();
+    struct Point *secondPoint = newPoint();
 
-    for (int i=0; i<2*n-1; i++){
-        struct Point beginPoint = point.blockCorners[i];
-        struct Point endPoint = point.blockCorners[i+1];
+    for (int i=0; i<3; i++){
+
+        copy_Point(beginPoint, &point->blockCorners[i]);
+        copy_Point(endPoint, &point->blockCorners[(i+1)%(2*n)]);
+        copy_Point(secondPoint, &point->blockCorners[(i+2)%(2*n)]);
 
         for (int j=0; j<n; j++)
         {
-            double newDistance = abs(beginPoint.coordinates[j]) + abs(endPoint.coordinates[j]);
+            double distance1 = beginPoint->coordinates[j];
+            double distance2 = endPoint->coordinates[j];
+            double newDistance = abs(distance1) + abs(distance2);
 
             if(newDistance>longestDistance) {
+
                 longestDistance = newDistance;
-                beginFinalPoint.coordinates = beginPoint.coordinates;
-                endFinalPoint.coordinates = endPoint.coordinates;
-                if(beginFinalPoint.coordinates[j] >=0) {
-                    beginFinalPoint.coordinates[j] = beginPoint.coordinates[j]-longestDistance/2;
+                copy_Point(firstDevisionPoint,endPoint);
+
+                if(firstDevisionPoint->coordinates[j] >= 0) {
+                    firstDevisionPoint->coordinates[j] = firstDevisionPoint->coordinates[j]-longestDistance/2;
                 } else {
-                    beginFinalPoint.coordinates[j] = beginPoint.coordinates[j]+longestDistance/2;
+                    firstDevisionPoint->coordinates[j] = firstDevisionPoint->coordinates[j]+longestDistance/2;
                 }
-                if(endFinalPoint.coordinates[j] >=0) {
-                    endFinalPoint.coordinates[j] = endPoint.coordinates[j]-longestDistance/2;
+
+                copy_Point(secondDevisionPoint, secondPoint);
+
+                if(secondDevisionPoint->coordinates[j] >=0) {
+                    secondDevisionPoint->coordinates[j] = secondDevisionPoint->coordinates[j]-longestDistance/2;
                 } else {
-                    endFinalPoint.coordinates[j] = endPoint.coordinates[j]+longestDistance/2;
+                    secondDevisionPoint->coordinates[j] = secondDevisionPoint->coordinates[j]+longestDistance/2;
                 }
             }
         }
 
     }
 
-    struct weightPoint *newBlocks = (struct weightPoint*)malloc(devisionConstant*sizeof(struct weightPoint));
-    struct weightPoint newPoint;
-    newPoint.blockCorners = point.blockCorners;
-    newPoint.blockCorners[0] = beginFinalPoint;
-    newPoint.blockCorners[1] = endFinalPoint;
 
-    struct weightPoint newPoint2;
-    newPoint2.blockCorners = point.blockCorners;
-    newPoint2.blockCorners[2] = beginFinalPoint;
-    newPoint2.blockCorners[3] = endFinalPoint;
 
-    newBlocks[0] = newPoint;
-    newBlocks[1] = newPoint2;
+    cout << "firstDevisionPoint " << endl;
+    showPoint(*firstDevisionPoint);
+    cout << "secondDevisionPoint " << endl;
+    showPoint(*secondDevisionPoint);
+
+    weightPoint *newPoint = newWeightPoint();
+    weightPoint *newPoint2 = newWeightPoint();
+
+    copy_weightPoint(newPoint, point);
+    copy_weightPoint(newPoint2, point);
+
+    copy_Point(&(newPoint->blockCorners[0]), firstDevisionPoint);
+    copy_Point(&(newPoint->blockCorners[3]), secondDevisionPoint);
+
+    copy_Point(&(newPoint2->blockCorners[1]), firstDevisionPoint);
+    copy_Point(&(newPoint2->blockCorners[2]), secondDevisionPoint);
+
+    vector<weightPoint *> newBlocks;
+    newBlocks.push_back(newPoint);
+    newBlocks.push_back(newPoint2);
+
+    free(firstDevisionPoint);
+    free(secondDevisionPoint);
+    free(beginPoint);
+    free(endPoint);
+    free(secondPoint);
 
     return newBlocks;
-
 }
+
 int main()
 {
-    struct weightPoint examplePoint;
+    weightPoint *examplePoint = newWeightPoint();
     vector<struct Point> points;
 
-    struct Point point1;
-    point1.coordinates[0] = -40;
-    point1.coordinates[1] = -40;
-    points.push_back(point1);
-    struct Point point2;
-    point2.coordinates[0] = 40;
-    point2.coordinates[1] = -40;
-    points.push_back(point2);
-    struct Point point3;
-    point3.coordinates[0] = 40;
-    point3.coordinates[1] = 40;
-    points.push_back(point3);
-    struct Point point4;
-    point4.coordinates[0] = -40;
-    point4.coordinates[1] = 40;
-    points.push_back(point4);
+    Point *point1 = newPoint();
+    point1->coordinates[0] = -40;
+    point1->coordinates[1] = -40;
+    points.push_back(*point1);
+    Point *point2 = newPoint();
+    point2->coordinates[0] = 40;
+    point2->coordinates[1] = -40;
+    points.push_back(*point2);
+    Point *point3 = newPoint();
+    point3->coordinates[0] = 40;
+    point3->coordinates[1] = 40;
+    points.push_back(*point3);
+    Point *point4 = newPoint();
+    point4->coordinates[0] = -40;
+    point4->coordinates[1] = 40;
+    points.push_back(*point4);
 
-    examplePoint.blockCorners = points;
+    memcpy(&examplePoint->blockCorners, &points, sizeof(points));
 
-    double myValues[] = {0,0};
+    /*double myValues[] = {0,0};
     examplePoint.dimensionsValue.assign(myValues, myValues+n);
 
     double result = einstainMultiplication(examplePoint);
@@ -151,17 +237,12 @@ int main()
     cout << "result summation: " << result << endl;
 
     result = evaluationFunction(examplePoint);
-    cout << "result evaluationFunction: " << result << endl;
+    cout << "result evaluationFunction: " << result << endl;*/
 
-    struct weightPoint *devidedBlocks;
+    vector<weightPoint *> devidedBlocks;
     devidedBlocks = devideBlock(examplePoint);
-    struct Point newPoint;
 
-    for(int i=0;i<4;i++){
-        newPoint = devidedBlocks[0].blockCorners[i];
-        cout << "newPoint" << i<< " x: " << newPoint.coordinates[0] << endl;
-        cout << "newPoint" << i << " y: " << newPoint.coordinates[1] << endl;
-    }
+    showBlockCorners(*(devidedBlocks[0]));
 
     return 0;
 }
